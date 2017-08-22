0972f0a83e7298bec334a6be3a860580c51e99fa
Giovanni Palusa <giovanni@palusa.se>
2017-08-22 10:57:24 +0200

    Initial Commit

---
+++ Cards4Humanity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cards4Humanity
+//
+//  Created by Giovanni Palusa on 2017-08-22.
+//  Copyright © 2017 Giovanni Palusa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Cards4HumanityTests/Cards4HumanityTests.swift
@@ -0,0 +1,36 @@
+//
+//  Cards4HumanityTests.swift
+//  Cards4HumanityTests
+//
+//  Created by Giovanni Palusa on 2017-08-22.
+//  Copyright © 2017 Giovanni Palusa. All rights reserved.
+//
+
+import XCTest
+@testable import Cards4Humanity
+
+class Cards4HumanityTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
