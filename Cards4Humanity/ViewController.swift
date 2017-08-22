031d3a59e7d8655775dcea02dd36d18962d13401
Giovanni Palusa <giovanni@palusa.se>
2017-08-22 14:38:29 +0200

    Main layout fixed in landscape and portrait mode

--- Cards4Humanity/ViewController.swift
+++ Cards4Humanity/ViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dealerBtn: UIButton!
+    @IBOutlet weak var leftCard: UIImageView!
+    @IBOutlet weak var rightCard: UIImageView!
+    @IBOutlet weak var playerScore: UILabel!
+    @IBOutlet weak var CPUScore: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
