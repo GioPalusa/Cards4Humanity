3f5f890df7851f278e9acfa98ae01e80a33218f5
Giovanni Palusa <giovanni@palusa.se>
2017-08-23 09:40:27 +0200

    Added second view with blur and final screen. No data transfer.

--- Cards4Humanity/ViewController.swift
+++ Cards4Humanity/ViewController.swift
@@ -19,11 +19,61 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    var playerScoreNumber = 0
+    var CPUScoreNumber = 0
     
-
-
+    
+    @IBAction func dealBtnPressed(_ sender: Any) {
+        let cardsArray = ["ace", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king"]
+        
+        if playerScoreNumber == 0 && CPUScoreNumber == 0 {
+            self.timer()
+        }
+        
+        let CPURandom = Int(arc4random_uniform(13))
+        let playerRandom = Int(arc4random_uniform(13))
+        
+        let playerCard = cardsArray[playerRandom]
+        let CPUCard = cardsArray[CPURandom]
+        
+        leftCard.image = UIImage(named: playerCard)
+        rightCard.image = UIImage(named: CPUCard)
+        
+        if playerRandom > CPURandom || playerRandom == 0 && CPURandom != 0 {
+            playerScoreNumber += 1
+        } else if playerRandom == CPURandom {
+            //do something
+        } else {
+            CPUScoreNumber += 1
+        }
+        
+        playerScore.text = ("\(playerScoreNumber)")
+        CPUScore.text = ("\(CPUScoreNumber)")
+    }
+    
+    func timer() {
+        _ = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(self.finishedRound), userInfo: nil, repeats: false);
+    }
+    
+    @objc func finishedRound() {
+        print("TIMER FINISHED")
+        playerScoreNumber = 0
+        CPUScoreNumber = 0
+        playerScore.text = ("\(playerScoreNumber)")
+        CPUScore.text = ("\(CPUScoreNumber)")
+        performSegue(withIdentifier: "endOfGame", sender: self)
+    }
+    
 }
+
+
+
+
+
+
+
+
+
 
